98c9f832a90b0f91013f87f94303cff74f860a27
geonjinchang <chang.58ki@gmail.com>
2017-03-03 09:44:28 +0000

    user profile update2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update]
+#  before_action :set_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+#    @user = User.find(params[:id])
   end
   
   def update
@@ -39,9 +40,9 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+#  def set_user
+#    @user = User.find(params[:id])
+#  end
   
   def correct_user
     @user = User.find(params[:id])
