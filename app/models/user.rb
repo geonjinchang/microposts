d06c1462f91b24532eb5e01baf224ddfd2fff3f0
geonjinchang <chang.58ki@gmail.com>
2017-03-13 04:16:28 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,9 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  
+  # タイムラインを取得するメソッド
+  def feed_items
+    Micropost.shere(user_id: following_user_ids + [self.id])
+  end
 end
