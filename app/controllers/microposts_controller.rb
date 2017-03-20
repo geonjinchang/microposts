6ca7081494649b84c81a2e5c779a20c2ad761e91
geonjinchang <chang.58ki@gmail.com>
2017-03-20 03:58:12 +0000

    fav pagenation

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "Micropost created!"
             redirect_to root_url
         else
-            @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+            @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
             @microposts = Micropost.order(:created_at).page params[:id]
             render 'static_pages/home'
         end
