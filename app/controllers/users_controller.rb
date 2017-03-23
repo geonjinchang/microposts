5bd8f238a0aaaff5580d0499ad4f2ff3a2c79fa5
geonjinchang <chang.58ki@gmail.com>
2017-03-23 08:51:54 +0000

    fix user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-#  before_action :set_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
   def show
@@ -48,12 +48,11 @@ class UsersController < ApplicationController
   
   private
 
-#  def set_user
-#    @user = User.find(params[:id])
-#  end
+  def set_user
+    @user = User.find(params[:id])
+  end
   
   def correct_user
-    @user = User.find(params[:id])
     if @user != current_user
       redirect_to root_url
     end
